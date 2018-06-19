2046c0dd2432029a4b13b4a946f7ef4c94f7bdab
aleks-rt <rita_aleks@mail.ru>
2018-06-19 11:55:37 +0300

    commit1

--- app/src/main/java/com/example/harrix/jump/Draw.kt
+++ app/src/main/java/com/example/harrix/jump/Draw.kt
@@ -7,63 +7,59 @@ import android.graphics.Paint
 import android.graphics.Point
 import android.view.View
 
-class Draw (context : Context) : View(context){
+class Draw (context : Context) : View(context) {
 
     var touch = false
-    var dy : Int = 8//разница в высоте между перерисовками
-    var dh : Int = 0//изменение высоты
-    var hOfJump : Int = 150//высоты прыжка
+    var dy: Int = 20//разница в высоте между перерисовками
+    var dh: Int = 0//изменение высоты
+    var hOfJump: Int = 150//высоты прыжка
 
-    var x = 50
+    var x = -1
     val dx = 2
+    var y = -1
 
     override fun onDraw(canvas: Canvas?) {
 
         super.onDraw(canvas)
-
         if (canvas == null) return
+        var y0 = canvas.height
+        if (x == -1) x = canvas.width
+        if (y == -1) y = canvas.height
+
 
         var paint = Paint()
 
-        /*fun drawTriangle(triangle: Triangle){
-            val p1 : Point = Point(triangle.x-triangle.w,triangle.y)
-            val p2 = Point(triangle.x + triangle.w,triangle.y)
-            val p3 = Point(triangle.x,triangle.y + triangle.orient*triangle.h)
-            canvas.drawLine(p1.x.toFloat(),p1.y.toFloat(),p2.x.toFloat(),p2.y.toFloat(),paint)
-            canvas.drawLine(p1.x.toFloat(),p1.y.toFloat(),p3.x.toFloat(),p3.y.toFloat(),paint)
-            canvas.drawLine(p2.x.toFloat(),p2.y.toFloat(),p3.x.toFloat(),p3.y.toFloat(),paint)
+
+        fun drawTriangle(triangle: Triangle) {
+            val p1: Point = Point(triangle.x - triangle.w, triangle.y)
+            val p2 = Point(triangle.x + triangle.w, triangle.y)
+            val p3 = Point(triangle.x, triangle.y + triangle.orient * triangle.h)
+            canvas.drawLine(p1.x.toFloat(), p1.y.toFloat(), p2.x.toFloat(), p2.y.toFloat(), paint)
+            canvas.drawLine(p1.x.toFloat(), p1.y.toFloat(), p3.x.toFloat(), p3.y.toFloat(), paint)
+            canvas.drawLine(p2.x.toFloat(), p2.y.toFloat(), p3.x.toFloat(), p3.y.toFloat(), paint)
         }
 
-        x += dx
+        x -= dx
+        drawTriangle(Triangle(x, y, -1))
 
-        if(touch) {
-            drawTriangle(Triangle(x, x, 1))
-            touch = false
-        }else {
-            drawTriangle(Triangle(x, 0, 1))
-
-        }*/
-        var y0 = canvas.height;
         if (touch) {
             dh += dy;
             if (dh >= hOfJump)
                 dy *= -1
-            canvas.drawCircle((canvas.width / 2).toFloat(), (y0 - dh - 10).toFloat(), 10f, paint)
+            paint.setColor(Color.BLUE)
+            canvas.drawCircle((canvas.width / 2).toFloat(), (y0 - dh - 10).toFloat(), 20f, paint)
             //отрисовка объекта
             //проверка препядствий
-            if (dh == 0)
-            {
+            if (dh == 0) {
                 //dy *= -1
                 touch = false
             }
-        }
-        else{
+        } else {
+            paint.setColor(Color.BLUE)
             dy = 1
-            canvas.drawCircle((canvas.width / 2).toFloat(), (y0 - 10).toFloat(), 10f, paint)
+            canvas.drawCircle((canvas.width / 2).toFloat(), (y0 - 10).toFloat(), 20f, paint)
             //отрисовка объекта
         }
         invalidate()
-
     }
-
 }
--- app/src/main/java/com/example/harrix/jump/Triangle.kt
+++ app/src/main/java/com/example/harrix/jump/Triangle.kt
@@ -1,8 +1,12 @@
 package com.example.harrix.jump
 
+import android.graphics.Paint
+
 class Triangle (var x : Int = 0,
                 var y : Int = 0,
                 var orient : Int = 1){
-    val h = 100
+    val h = 70
     val w = 30
+    var paint= Paint()
+
 }
